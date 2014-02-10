746f00b1ab02c9192428ff3a8b77b0a045a4f29b
Ahmed Ezzat Afifi <AEzzatA@gmail.com>
2014-02-10 16:54:28 +0200

    Adding naive views, and their correspondent urls, soon to be changed

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+	return HttpResponse("Hello, world. you're at the poll index.")
+
+def detail(request, poll_id):
+	return HttpResponse("You're looking at the poll {0}".format(poll_id))
+
+def results(request, poll_id):
+	return HttpResponse("You're looking at the results of poll {0}".format(poll_id))
+
+def vote(request, poll_id):
+	return HttpResponse("You're voting on poll {0}".format(poll_id))
