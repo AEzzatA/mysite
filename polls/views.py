090d6ccaf14a326c939b6b3a4bb28e3e901a68cc
Ahmed Ezzat Afifi <AEzzatA@gmail.com>
2014-02-15 11:52:36 +0200

    Finished part 5

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from polls.models import Poll, Choice
 from django.template import RequestContext, loader
 from django.core.urlresolvers import reverse
+from django.utils import timezone
 
 from django.views import generic
 # Create your views here.
@@ -13,11 +14,16 @@ class IndexView(generic.ListView):
 
 	def get_queryset(self):
 		'''Return the last five published polls'''
-		return Poll.objects.order_by('-pub_date')[:5]
+		return Poll.objects.filter(pub_date__lte=timezone.now()
+			).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
 	model = Poll
 	template_name = 'polls/detail.html'
+
+	def get_queryset(self):
+		'''Excludes any polls that aren't published yet.'''
+		return Poll.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
 	model = Poll
